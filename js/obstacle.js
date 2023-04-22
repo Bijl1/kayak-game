0d7fd9e6cc817dc21863e24ce8ffb5bf748159db
Jonathan Bijl <sannoayo@gmail.com>
2023-04-22 12:59:51 -0400

    powerup JS

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,32 +1,49 @@
 class Obstacle {
     constructor(gameScreen) {
+      // the game screen where the obstacle will be displayed
       this.gameScreen = gameScreen;
-
-      this.left = Math.floor(Math.random() * 250);
-      this.top =   5;
-      this.width = 75;
-      this.height = 100;
-      this.speed = 3;
+  
+      // set the initial position, dimensions, and speed of the obstacle
+      this.left = Math.floor(Math.random() * 501); // randomize position
+      this.top = 5; // set initial top position
+      this.width = 75; // set the width of the obstacle
+      this.height = 100; // set the height of the obstacle
+      this.speed = 3; // set the initial speed of the obstacle
+      this.speedIncrement = 1; // set the increment of the speed of the obstacle
+  
+      // create an img element for the obstacle and set its attributes
       this.element = document.createElement("img");
       this.element.src = "./images/gator.png";
-      
       this.element.style.position = "absolute";
-
       this.element.style.width = `${this.width}px`;
       this.element.style.height = `${this.height}px`;
       this.element.style.left = `${this.left}px`;
       this.element.style.top = `${this.top}px`;
-
+  
+      // append the obstacle to the game screen
       this.gameScreen.appendChild(this.element);
+  
+      // set a flag to indicate if the obstacle has passed a certain point
+      this.hasPassedPoint = false;
     }
-
+  
+    // update the position of the obstacle on the game screen
     updatePosition() {
       this.element.style.left = `${this.left}px`;
       this.element.style.top = `${this.top}px`;
     }
-    
+  
+    // move the obstacle down the game screen
     move() {
-        this.top += this.speed;
+      this.top += this.speed;
       this.updatePosition();
+  
+      // if the obstacle has passed a certain point on the game screen,
+      // increase its speed and set the flag to true
+      if (this.top > 600 && !this.hasPassedPoint) {
+        this.speed += this.speedIncrement; // increase speed
+        this.hasPassedPoint = true; // set flag to true
+      }
     }
-}
+  }
+  
--- js/player.js
+++ js/player.js
@@ -1,72 +1,78 @@
 class Player {
     constructor(gameScreen, left, top, width, height, imgSrc) {
-      this.gameScreen = gameScreen;
-      this.left = left;
-      this.top = top;
-      this.width = width;
-      this.height = height;
-      this.directionX = 50;
-      this.directionY = 50;
-  
-      this.element = document.createElement("img");
-      this.element.src = imgSrc;
-  
-      this.element.style.position = "absolute";
-  
-      this.element.style.width = `${this.width}px`;
-      this.element.style.height = `${this.height}px`;
-      this.element.style.left = `${this.left}px`;
-      this.element.style.top = `${this.top}px`;
-  
-      this.gameScreen.appendChild(this.element);
+        // Set the game screen element
+        this.gameScreen = gameScreen;
+        // Set the initial position and dimensions of the player
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        // Set the initial movement direction of the player
+        this.directionX = 50;
+        this.directionY = 50;
+
+        // Create an image element for the player
+        this.element = document.createElement("img");
+        this.element.src = imgSrc;
+
+        // Set the CSS styles for the player element
+        this.element.style.position = "absolute";
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+
+        // Append the player element to the game screen element
+        this.gameScreen.appendChild(this.element);
     }
-  
+
+    // Method to move the player element
     move() {
-      this.left += this.directionX;
-      this.top += this.directionY;
-  
-      // sets boundary to left hand side
-      if(this.left < 30){
-        this.left = 30;
-      }
-  
-      // sets boundary to top side
-      if (this.top < 10){
-        this.top = 10;
-      }
-  
-      // sets boundary to right hand side
-      if(this.left + this.width > this.gameScreen.offsetWidth - 30){
-        this.left = this.gameScreen.offsetWidth - this.width - 30;
-      }
-  
-      // sets boundary to down side
-      if(this.top + this.height > this.gameScreen.offsetHeight - 10){
-        this.top = this.gameScreen.offsetHeight - this.height - 10;
-      }    
-  
-      this.updatePosition();
+        // Update the position of the player element
+        this.left += this.directionX;
+        this.top += this.directionY;
+
+        // Set boundaries for the player element
+        if (this.left < 30) {
+            this.left = 30;
+        }
+        if (this.top < 10) {
+            this.top = 10;
+        }
+        if (this.left + this.width > this.gameScreen.offsetWidth - 10) {
+            this.left = this.gameScreen.offsetWidth - this.width - 10;
+        }
+        if (this.top + this.height > this.gameScreen.offsetHeight - 10) {
+            this.top = this.gameScreen.offsetHeight - this.height - 10;
+        }
+
+        // Update the position of the player element
+        this.updatePosition();
     }
-  
+
+    // Method to update the CSS position of the player element
     updatePosition() {
-      this.element.style.left = `${this.left}px`;
-      this.element.style.top = `${this.top}px`;
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
     }
-  
+
+    // Method to detect collisions with an obstacle element
     didCollide(obstacle) {
-      const playerRect = this.element.getBoundingClientRect();
-      const obstacleRect = obstacle.element.getBoundingClientRect();
-  
-      if(
-        playerRect.left < obstacleRect.right &&
-        playerRect.right > obstacleRect.left && 
-        playerRect.top < obstacleRect.bottom &&
-        playerRect.bottom > obstacleRect.top
-      ){
-        console.log("COLLISION@ 💥💥💥💥💥");
-        return true
-      } else {
-        return false;
-      }
+        // Get the bounding rectangles for the player and obstacle elements
+        const playerRect = this.element.getBoundingClientRect();
+        const obstacleRect = obstacle.element.getBoundingClientRect();
+
+        // Check if the player and obstacle rectangles overlap
+        if (
+            playerRect.left < obstacleRect.right &&
+            playerRect.right > obstacleRect.left &&
+            playerRect.top < obstacleRect.bottom &&
+            playerRect.bottom > obstacleRect.top
+        ) {
+            console.log("COLLISION@ 💥💥💥💥💥");
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
+}
